22c382ff106d181995f5e9913adbdf0a76373a31
Adliano Alves <adliano@me.com>
2019-10-19 00:11:14 -0700

    added fetchPaverData() and loadTypeOptions()

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,27 +1,47 @@
-
 const CALSTONE_URL = 'data/calstone.json'
+const BELGARD_URL = 'data/belgard.json'
 
-document.getElementById('brandSelector').addEventListener('change', loadData)
-//  (event => {
-//     console.log(event.target.value);
-// }))
+// document.querySelector('#typeSelector').innerHTML =
+//   '<option value="select">Select...</option><option value="select">Select...</option><option value="select">Select...</option>'
 
-function loadData (e) {
-    console.log(event.target.value);
-    // Fetch data by brand
-
+function fetchPaverData (brandURL) {
+  fetch(brandURL)
+    .then(results => results.json())
+    .then(data => loadTypeOptions(data))
 }
 
-fetch(CALSTONE_URL)
-.then(results => results.json())
-.then(data => {
-    const objKeys = Object.keys(data)
-    console.log(data)
-    console.log(objKeys)
-    
-})
+function loadTypeOptions(paverData){
+    let options = paverData.pavers.map((item, index) => {
+        return `<option value="${item.name}">${item.name}</option>`
+    })
+    document.querySelector('#typeSelector').innerHTML = options
+}
+
+// typeSelector
+function onBrandChange (e) {
+  const selectedBrand = event.target.value.toLowerCase()
+  let URL = ''
+
+  switch (selectedBrand) {
+    case 'belgard':
+      console.log('Belgard selected')
+      URL = 'data/belgard.json'
+      break
+    case 'calstone':
+      console.log('Calstone selected')
+      URL = 'data/calstone.json'
+      break
+    default:
+      console.log('Basalite selected')
+      break
+  }
+
+  fetchPaverData(URL)
+}
+
+document
+  .getElementById('brandSelector')
+  .addEventListener('change', onBrandChange)
 
 // console.log(JSON.stringify(patterns))
 // console.log(patterns.QS1)
-
-
