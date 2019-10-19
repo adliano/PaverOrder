5de87fa8141c623e9fd38750c854ff8af298cf83
Adliano Alves <adliano@me.com>
2019-10-18 19:47:46 -0700

    added be;gard json data

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,5 @@
 
-const CALSTONE_URL = 'data/Calstone/calstone.json'
+const CALSTONE_URL = 'data/calstone.json'
 
 document.getElementById('brandSelector').addEventListener('change', loadData)
 //  (event => {
@@ -9,14 +9,19 @@ document.getElementById('brandSelector').addEventListener('change', loadData)
 function loadData (e) {
     console.log(event.target.value);
     // Fetch data by brand
-       
+
 }
 
-// fetch(CALSTONE_URL)
-// .then(results => results.json())
-// .then(data => {
-//     const objKeys = Object.keys(data)
-//     console.log(data)
-//     console.log(objKeys)
+fetch(CALSTONE_URL)
+.then(results => results.json())
+.then(data => {
+    const objKeys = Object.keys(data)
+    console.log(data)
+    console.log(objKeys)
     
-// })
+})
+
+// console.log(JSON.stringify(patterns))
+// console.log(patterns.QS1)
+
+
