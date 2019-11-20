35284d4e06e2097ef9c65262a1c2918a45b17734
Adliano Alves <adliano@me.com>
2019-11-19 23:13:21 -0800

    working on calcula te pavers

--- assets/javascript/util.js
+++ assets/javascript/util.js
@@ -35,3 +35,38 @@ export const loadTypeOptions = (paverData, domElement) => {
   options.unshift('<option>Select...</option>')
   domElement.innerHTML = options
 }
+/*************************************/
+/**
+ * @method getBorderMultiplier()
+ * This method will accept size and course of the border
+ * and return the multiplier
+ * @param {String} _borderSize
+ * @param {String} _borderCourse
+ * @returns {Number} multiplier
+ */
+/*************************************/
+const getBorderMultiplier = (_borderSize, _borderCourse) => {
+  const _borderDimentions = _borderSize
+    .toLowerCase()
+    .split('x')
+    .map(item => parseInt(item)) // Change string to number
+    .sort() // sort it so smaller size will be array[0]
+  // return the border multiplier
+  return _borderCourse === 'Soldier'
+    ? _borderDimentions[1] / 12
+    : _borderDimentions[0] / 12
+}
+/**
+ * ***********************************
+ * @method calculatePavers()
+ * @param {JSON} paverObj
+ *  ***********************************
+ */
+export const calculatePavers = (stateObj) => {
+  console.log(stateObj)
+  const { totalSQF, } = stateObj.paver
+  const { totalLF } = stateObj.border
+
+  // const borderSQF = parseInt(totalLF) * getBorderMultiplier(borderSize, borderCourse) 
+  // TODO: get border size and course
+}
